5290770b1a253e6b67e42c872f1ce741af9ab4e5
Amisha Singh <amishaaworks06@gmail.com>
2026-02-06 04:04:34 +0530

    Add Login and Profile routes to Body using outlet and include Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<div>Base Page</div>} />
-          <Route path="/login" element={<div>Login Page</div>} />
-          <Route path="/test" element={<div>Test Page</div>} />
+          <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
-      {/* <Navbar /> */}
-      {/* <h1 class="text-3xl font-bold underline text-indigo-200">Hello world!</h1> */}
     </>
   );
 }
