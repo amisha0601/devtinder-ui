962926be00b813d0fdb0ca02a449f3e6c3829894
Amisha Singh <amishaaworks06@gmail.com>
2026-02-05 23:08:58 +0530

    install and setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1 class="text-3xl font-bold underline text-indigo-200">Hello world!</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>Base Page</div>} />
+          <Route path="/login" element={<div>Login Page</div>} />
+          <Route path="/test" element={<div>Test Page</div>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Navbar /> */}
+      {/* <h1 class="text-3xl font-bold underline text-indigo-200">Hello world!</h1> */}
     </>
   );
 }
