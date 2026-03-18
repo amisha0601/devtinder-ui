6724bc51c20c698014209c53c3fb4ceedee424dd
Amisha Singh <amishaaworks06@gmail.com>
2026-03-18 16:31:27 +0530

    implement handleSendRequest to manage Interested and Ignore actions

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { BASE_URL } from "../utils/constants";
 import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
 import UserCard from "./UserCard";
 
 const Feed = () => {
@@ -10,23 +10,37 @@ const Feed = () => {
   const dispatch = useDispatch();
 
   const getFeed = async () => {
-    if (feed) return;
+    if (feed && feed.length > 0) return;
+
     try {
-      const res = await axios.get(BASE_URL + "/feed", {withCredentials : true});
-      dispatch(addFeed(res.data));
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+
+      const feedData = res?.data?.data || res?.data;
+      dispatch(addFeed(feedData));
     } catch (err) {
-      console.error("Error fetching feed:", err);
+      console.error("Feed Error:", err);
     }
   };
 
   useEffect(() => {
     getFeed();
-  },[])
+  }, []);
 
-  return feed && (
-  <div className="flex justify-center my-10">
-    <UserCard user = {feed[0]} />
-  </div>
+  if (!feed) return;
+
+  if (feed.length === 0)
+    return (
+      <h1 className="flex justify-center my-10 text-white">
+        No new users found!
+      </h1>
+    );
+
+  return (
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]} />
+    </div>
   );
 };
 
