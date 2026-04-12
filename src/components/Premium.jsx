963a58e8ea91da34355e258d38828dad10429f05
Amisha Singh <amishaaworks06@gmail.com>
2026-04-13 02:14:54 +0530

    check and handle premium membership status

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,38 +1,60 @@
 import React from "react";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
   const handleBuyClick = async (type) => {
-    const order = await axios.post(BASE_URL + "/payment/create", {
-     membershipType: type,
+    const order = await axios.post(
+      BASE_URL + "/payment/create",
+      {
+        membershipType: type,
+      },
+      { withCredentials: true },
+    );
 
-    }, {withCredentials: true})
+    const { amount, keyId, currency, notes, orderId } = order.data;
 
-      const {amount, keyId, currency, notes, orderId} = order.data;
+    const options = {
+      key: keyId,
+      amount,
+      currency,
+      name: "devTinder",
+      description: "Connect with other developers",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#F37254",
+      },
+      handler: verifyPremiumUser,
+    };
 
-      const options = {
-        key:keyId,
-        amount,
-        currency,
-        name: 'devTinder',
-        description: 'Connect with other developers',
-        order_id:orderId,
-        prefill: {
-          name: notes.firstName + " " + notes.lastName,
-          email:notes.emailId,
-          contact: '9999999999'
-        },
-        theme: {
-          color: '#F37254'
-        },
-      };
-
-     const rzp = new window.Razorpay(options);
-      rzp.open();
-
-  }
-  return (
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+  return isUserPremium ? (
+    "You're are already a premium user"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -43,7 +65,12 @@ const Premium = () => {
             <li> - Blue Tick</li>
             <li> - 3 months</li>
           </ul>
-          <button onClick={() => handleBuyClick("gold")} className="btn btn-secondary">Buy Silver</button>
+          <button
+            onClick={() => handleBuyClick("gold")}
+            className="btn btn-secondary"
+          >
+            Buy Silver
+          </button>
         </div>
         <div className="divider divider-horizontal">OR</div>
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
